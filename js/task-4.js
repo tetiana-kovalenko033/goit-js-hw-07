e4d5e89fe1044655b677611aed49401ece79b2b8
Tetiana <kovalenko_t@ukr.net>
2025-03-09 15:24:08 +0200

    fix task 4

--- js/task-4.js
+++ js/task-4.js
@@ -10,11 +10,12 @@ function handleSubmit(event) {
   const password = form.elements.password.value;
   
   if (email === "" || password === "") {
-    alert `All  form fields must be filled in`;
+    alert`All  form fields must be filled in`;
+    return;
   }
     const result = {};
-    result.email = email;
-    result.password = password;
+    result.email = email.trim();
+    result.password = password.trim();
 
   console.log(result);
   form.reset();
